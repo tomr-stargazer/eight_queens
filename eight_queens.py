cb42405409beffbcd4351e4e519354ac4dd06d13
Tom Rice <t.rice90@gmail.com>
2015-06-10 22:55:15 -0400

    I solved the eight queens problem!

--- eight_queens.py
+++ eight_queens.py
@@ -88,3 +88,49 @@ def is_board_legal(queen_positions):
         return False
     else:
         return True
+
+
+def eight_queen_solver(input_board=None, size=8):
+
+    # initialize:
+    if input_board is None:
+        input_board = np.zeros((size, size))
+
+    # check to see if we win:
+    if ((np.sum(input_board.astype(np.bool)) == size) and 
+        is_board_legal(input_board)):
+        return input_board
+    elif ((np.sum(input_board.astype(np.bool)) == size) and 
+          ~is_board_legal(input_board)):
+        raise ValueError("Somehow we got an illegal board as a result")
+
+    threatened_squares = multiple_queens_threatened_squares(input_board)
+
+    # check to see if we lose:
+    if (input_board.astype(np.bool)+threatened_squares).all():
+        return False
+
+    # try all legal positions, put a queen there, and see how far we can get
+    empty_unthreatened_squares = ~(input_board.astype(np.bool)+threatened_squares)
+    x_positions, y_positions = np.where(empty_unthreatened_squares)
+
+    for x, y in zip(x_positions, y_positions):
+
+        queen_number = int(np.max(input_board)+1)
+
+        print "Placing queen #{2} at {0}, {1}".format(x, y, queen_number)
+
+        board_with_new_queen = np.copy(input_board)
+        board_with_new_queen[x, y] = queen_number
+
+        # print board_with_new_queen
+
+        result = eight_queen_solver(input_board=board_with_new_queen, size=size)
+
+        if (result is not False) and (result is not None):
+            return result
+        else:
+            print "Queen #{2} at {0}, {1} was a dead-end".format(x, y, queen_number)
+            # return False
+        # else:
+        #     raise ValueError("Failed to find a solution for size={0}".format(size))
