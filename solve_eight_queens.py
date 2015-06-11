f35c92a1af92dc53fbeb891c6eb8d3706f3693d4
Tom Rice <t.rice90@gmail.com>
2015-06-10 23:10:41 -0400

    here's a little script that you can invoke from the command line

---
+++ solve_eight_queens.py
@@ -0,0 +1,13 @@
+"""
+A script that calls `eight_queen_solver` and prints the result.
+
+"""
+
+from eight_queens import eight_queen_solver
+
+result = eight_queen_solver(size=8)
+
+print ""
+print "Solution:"
+print ""
+print result
