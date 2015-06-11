001d1fc28daf6887c4f1b3cc2c6dc7feb98b2da2
Tom Rice <t.rice90@gmail.com>
2015-06-10 21:39:19 -0400

    wrote the first piece of this puzzle & it's tested too

--- eight_queens.py
+++ eight_queens.py
@@ -1,4 +1,28 @@
 """
 Ok, so let's attempt to solve the 8 queens problem. (like, brute-force-ly.)
 
+My understanding of the eight queens problem: try to put 8 queens on a 
+chess board that don't threaten each other chess-ly.
+
 """
+
+import numpy as np
+
+chess_board = np.zeros((8,8))
+
+# let's try first an "eight rooks" problem?
+
+def rook_threatened_squares(x, y, board=chess_board):
+    """
+    Takes an x,y position and returns the threatened squares as True.
+
+    """
+
+    x_indices, y_indices = np.indices(board.shape)
+
+    # threatened squares are where either x or y is equal, but NOT BOTH (because that's the square you're on)
+    threatened = (((x_indices == x) | (y_indices == y)) & 
+                 ~((x_indices == x) & (y_indices == y)))
+
+    return threatened
+
---
+++ test_eight_queens.py
@@ -0,0 +1,28 @@
+"""
+Here are some tests for the eight_queens thing.
+
+"""
+
+import numpy as np
+from numpy.testing import assert_equal
+
+from .eight_queens import rook_threatened_squares
+
+def test_rook_threatened_squares():
+
+    # smaller cause I'm too lazy to type an expected 8x8 board
+    test_board = np.zeros((3,3))
+
+    expected_threatened = np.array([
+        [False, True, False],
+        [True, False, True],
+        [False, True, False]
+        ])
+
+    threatened = rook_threatened_squares(1,1, board=test_board)
+
+    assert_equal(threatened, expected_threatened)
+
+    
+
+
